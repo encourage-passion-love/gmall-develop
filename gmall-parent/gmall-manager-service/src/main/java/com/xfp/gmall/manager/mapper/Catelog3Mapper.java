a3d7342e03be8337a564d82bbcd6979c65c857c0
徐凤鹏 <951794968@qq.com>
2022-11-22 19:03:30 +0800

    商品一级分类生产者端接口逻辑和SQL编写

--- gmall-parent/gmall-manager-service/src/main/java/com/xfp/gmall/manager/mapper/Catelog3Mapper.java
+++ gmall-parent/gmall-manager-service/src/main/java/com/xfp/gmall/manager/mapper/Catelog3Mapper.java
@@ -1,4 +1,11 @@
 package com.xfp.gmall.manager.mapper;
 
+import com.xfp.gmall.manager.bean.PmsBaseCatalog2;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog3;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface Catelog3Mapper {
+    List<PmsBaseCatalog3> getCateLog3(@Param("catalog2_id") String catelog2_id);
 }
--- gmall-parent/gmall-manager-service/src/main/java/com/xfp/gmall/manager/service/CatelogServiceImpl.java
+++ gmall-parent/gmall-manager-service/src/main/java/com/xfp/gmall/manager/service/CatelogServiceImpl.java
@@ -1,8 +1,12 @@
 package com.xfp.gmall.manager.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog2;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog3;
 import com.xfp.gmall.manager.mapper.Catelog1Mapper;
 import com.xfp.gmall.manager.bean.PmsBaseCatalog1;
+import com.xfp.gmall.manager.mapper.Catelog2Mapper;
+import com.xfp.gmall.manager.mapper.Catelog3Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -12,10 +16,27 @@ public class CatelogServiceImpl implements CatelogService {
 
     @Autowired
     private Catelog1Mapper catelog1Mapper;
+    @Autowired
+    private Catelog2Mapper catelog2Mapper;
+    @Autowired
+    private Catelog3Mapper catelog3Mapper;
 
     @Override
     public List<PmsBaseCatalog1> getCatelog1() {
         List<PmsBaseCatalog1> cateLog1 = catelog1Mapper.getCateLog1();
         return cateLog1;
+    }
+
+    @Override
+    public List<PmsBaseCatalog2> getCatelog2(String catelog1_id) {
+        List<PmsBaseCatalog2> cateLog2 = catelog2Mapper.getCateLog2(catelog1_id);
+        return cateLog2;
+    }
+
+
+    @Override
+    public List<PmsBaseCatalog3> getCatelog3(String catelog2_id) {
+        List<PmsBaseCatalog3> cateLog3 = catelog3Mapper.getCateLog3(catelog2_id);
+        return cateLog3;
     }
 }
