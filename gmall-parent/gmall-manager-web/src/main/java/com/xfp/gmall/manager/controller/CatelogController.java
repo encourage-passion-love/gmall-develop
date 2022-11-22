053d30aa325d094f718139914afc4084743b77f9
徐凤鹏 <951794968@qq.com>
2022-11-22 19:04:18 +0800

    商品三级分类生产者端接口业务逻辑代码编写

--- gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/CatelogController.java
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/CatelogController.java
@@ -2,8 +2,11 @@ package com.xfp.gmall.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.xfp.gmall.manager.bean.PmsBaseCatalog1;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog2;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog3;
 import com.xfp.gmall.manager.service.CatelogService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +21,15 @@ public class CatelogController {
     public List<PmsBaseCatalog1> getCatalog1(){
         List<PmsBaseCatalog1> catelog1 = catelogService.getCatelog1();
         return catelog1;
+    }
+    @RequestMapping("/getCatalog2")
+    public List<PmsBaseCatalog2> getCatalog2(@RequestParam("catalog1Id") String catelog1Id){
+        List<PmsBaseCatalog2> catelog2 = catelogService.getCatelog2(catelog1Id);
+        return catelog2;
+    }
+    @RequestMapping("/getCatalog3")
+    public List<PmsBaseCatalog3> getCatalog3(@RequestParam("catalog2Id") String catelog2Id){
+        List<PmsBaseCatalog3> catelog3 = catelogService.getCatelog3(catelog2Id);
+        return catelog3;
     }
 }
--- gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/service/CatelogService.java
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/service/CatelogService.java
@@ -1,9 +1,13 @@
 package com.xfp.gmall.manager.service;
 
 import com.xfp.gmall.manager.bean.PmsBaseCatalog1;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog2;
+import com.xfp.gmall.manager.bean.PmsBaseCatalog3;
 
 import java.util.List;
 
 public interface CatelogService {
     public List<PmsBaseCatalog1> getCatelog1();
+    public List<PmsBaseCatalog2> getCatelog2(String catelog1_id);
+    public List<PmsBaseCatalog3> getCatelog3(String catelog2_id);
 }
