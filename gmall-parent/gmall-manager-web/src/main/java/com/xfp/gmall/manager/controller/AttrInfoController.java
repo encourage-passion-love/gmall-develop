1b262ce5f6951003593959e19633ee5afb62bfd4
徐凤鹏 <951794968@qq.com>
2022-11-22 22:16:14 +0800

    根据商品三级分类编号查询三级分类下对应的平台属性消费者端代码编写

---
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/AttrInfoController.java
@@ -0,0 +1,25 @@
+package com.xfp.gmall.manager.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.xfp.gmall.manager.service.PmsAttrService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import com.xfp.gmall.manager.bean.PmsBaseAttrInfo;
+@RestController
+public class AttrInfoController {
+
+    @Reference
+    private PmsAttrService pmsAttrService;
+
+    @RequestMapping("/attrInfoList")
+    public List<PmsBaseAttrInfo> attrInfoList(@RequestParam("catalog3Id") String catalog3Id){
+        List<PmsBaseAttrInfo> pmsBaseAttrInfos = pmsAttrService.attrInfoList(catalog3Id);
+        return pmsBaseAttrInfos;
+    }
+
+
+
+}
