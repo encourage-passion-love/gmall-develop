d2c25d3e41f73f3e7c0a8eea6c1dd1924f534dc8
徐凤鹏 <951794968@qq.com>
2022-11-25 20:29:30 +0800

    sku添加接口service层编写【消费者端】

--- gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/SkuController.java
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/SkuController.java
@@ -2,11 +2,19 @@ package com.xfp.gmall.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.xfp.gmall.manager.service.SkuService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.xfp.gmall.manager.bean.PmsSkuInfo;
 @RestController
 public class SkuController {
+
     @Reference
     private SkuService skuService;
+
+    @RequestMapping("/saveSkuInfo")
+    public PmsSkuInfo saveSkuInfo(@RequestBody PmsSkuInfo pmsSkuInfo){
+        PmsSkuInfo skuInfo = skuService.saveSkuInfo(pmsSkuInfo);
+        return skuInfo;
+    }
 }
--- gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/service/SkuService.java
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/service/SkuService.java
@@ -1,12 +1,14 @@
 package com.xfp.gmall.manager.service;
 
 import com.xfp.gmall.manager.bean.PmsProductInfo;
+import com.xfp.gmall.manager.bean.PmsSkuInfo;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface SkuService {
 
-
+    public PmsSkuInfo saveSkuInfo( PmsSkuInfo pmsSkuInfo);
 
 
 }
