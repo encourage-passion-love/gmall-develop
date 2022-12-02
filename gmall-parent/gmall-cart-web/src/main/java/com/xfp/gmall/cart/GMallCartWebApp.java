fceb5f88526f6a10f7e7e8d306a5915348e5fcb1
徐凤鹏 <951794968@qq.com>
2022-12-02 20:14:33 +0800

    购物车的业务逻辑【消费者端端】以及pom文件的引入

---
+++ gmall-parent/gmall-cart-web/src/main/java/com/xfp/gmall/cart/GMallCartWebApp.java
@@ -0,0 +1,11 @@
+package com.xfp.gmall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMallCartWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GMallCartWebApp.class,args);
+    }
+}
