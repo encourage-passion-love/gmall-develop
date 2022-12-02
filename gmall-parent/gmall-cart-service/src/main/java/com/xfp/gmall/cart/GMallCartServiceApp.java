aba41823dec726584d70a05a7b136950858f682f
徐凤鹏 <951794968@qq.com>
2022-12-02 23:01:08 +0800

    tkmapper{通用mapper扫描路径配置}

--- gmall-parent/gmall-cart-service/src/main/java/com/xfp/gmall/cart/GMallCartServiceApp.java
+++ gmall-parent/gmall-cart-service/src/main/java/com/xfp/gmall/cart/GMallCartServiceApp.java
@@ -2,8 +2,10 @@ package com.xfp.gmall.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.xfp.gmall.cart.mapper")
 public class GMallCartServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(GMallCartServiceApp.class,args);
