1229c424d32e5f924587705a51b4eaedc20d8113
徐凤鹏 <951794968@qq.com>
2022-12-02 23:11:06 +0800

    1.根据用户ID和商品的skuId查询数据库购物车表是否有这条数据2.数据购物车没有此项商品需要新添加商品3.购物车有此项商品需要修改此项商品在数据库中的数据

--- gmall-parent/gmall-cart-service/src/main/java/com/xfp/gmall/cart/service/CartItemServiceImpl.java
+++ gmall-parent/gmall-cart-service/src/main/java/com/xfp/gmall/cart/service/CartItemServiceImpl.java
@@ -2,22 +2,38 @@ package com.xfp.gmall.cart.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.xfp.gmall.cart.bean.OmsCartItem;
+import com.xfp.gmall.cart.mapper.CartItemMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 @Service
 public class CartItemServiceImpl implements CartItemService {
 
+    @Autowired
+    private CartItemMapper cartItemMapper;
+
     @Override
     public OmsCartItem getOmsCartItemByMemberIdAndSkuId(String memberId, String skuId) {
-        return null;
+        OmsCartItem omsCartItem = new OmsCartItem();
+        omsCartItem.setMemberId(memberId);
+        omsCartItem.setProductSkuId(skuId);
+        OmsCartItem omsCartItemResult = cartItemMapper.selectOne(omsCartItem);
+        return omsCartItemResult;
     }
 
     @Override
     public void saveCartItemToDB(OmsCartItem omsCartItem) {
-
+        if(StringUtils.isNotBlank(omsCartItem.getProductSkuId())){
+            cartItemMapper.insertSelective(omsCartItem);
+        }
     }
 
     @Override
     public void updateCartItemBySkuId(OmsCartItem omsCartFromDb) {
-
+        Example example=new Example(OmsCartItem.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("productSkuId",omsCartFromDb.getProductSkuId());
+        cartItemMapper.updateByExampleSelective(omsCartFromDb,example);
     }
 }
