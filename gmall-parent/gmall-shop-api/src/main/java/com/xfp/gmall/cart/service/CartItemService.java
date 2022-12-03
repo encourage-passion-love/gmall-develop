4b00d493841e261b9cb578ad2e29fefd2e3e67a3
徐凤鹏 <951794968@qq.com>
2022-12-03 11:49:25 +0800

    增加在数据库中查询用户的购物车数据

--- gmall-parent/gmall-shop-api/src/main/java/com/xfp/gmall/cart/service/CartItemService.java
+++ gmall-parent/gmall-shop-api/src/main/java/com/xfp/gmall/cart/service/CartItemService.java
@@ -2,8 +2,11 @@ package com.xfp.gmall.cart.service;
 
 import com.xfp.gmall.cart.bean.OmsCartItem;
 
+import java.util.List;
+
 public interface CartItemService {
     public  OmsCartItem getOmsCartItemByMemberIdAndSkuId(String memberId, String skuId);
     public void saveCartItemToDB(OmsCartItem omsCartItem);
     public void updateCartItemBySkuId(OmsCartItem omsCartFromDb);
+    public List<OmsCartItem> getCartItemByMemberId(String memberId);
 }
