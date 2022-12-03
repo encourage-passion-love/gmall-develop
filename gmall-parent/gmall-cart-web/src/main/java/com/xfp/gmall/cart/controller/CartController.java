cd8697646996d563ebb1eca28f35888c6525efc0
徐凤鹏 <951794968@qq.com>
2022-12-03 11:52:16 +0800

    用户购物车列表查询接口编写【消费者端】

--- gmall-parent/gmall-cart-web/src/main/java/com/xfp/gmall/cart/controller/CartController.java
+++ gmall-parent/gmall-cart-web/src/main/java/com/xfp/gmall/cart/controller/CartController.java
@@ -10,11 +10,15 @@ import com.xfp.gmall.util.RedisUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import redis.clients.jedis.Jedis;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.*;
@@ -44,19 +48,18 @@ public class CartController {
         omsCartItem.setProductName(skuInfo.getSkuName());
         omsCartItem.setProductPic(skuInfo.getSkuDefaultImg());
         String memberId = "1";
-        if(StringUtils.isBlank(memberId)){
+        if (StringUtils.isBlank(memberId)) {
             Cookie[] cookies = request.getCookies();
             Cookie cookie1 = null;
-            if(cookies!=null&&cookies.length>0){
+            if (cookies != null && cookies.length > 0) {
                 for (Cookie cookie : cookies) {
                     String name = cookie.getName();
                     if ("cartListCookie".equals(name)) {
                         cookie1 = cookie;
                     }
                 }
-            }
-            else {
-                cookie1=new Cookie("cartListCookie","");
+            } else {
+                cookie1 = new Cookie("cartListCookie", "");
             }
             String cookie1Value = cookie1.getValue();
             if (StringUtils.isBlank(cookie1Value)) {
@@ -64,15 +67,15 @@ public class CartController {
                 cookie1.setMaxAge(60 * 60 * 24 * 3);
                 cookie1.setDomain("localhost");
                 cookie1.setPath("/");
-                cookie1.setValue(URLEncoder.encode(JSON.toJSONString(omsCartItems),"UTF-8"));
+                cookie1.setValue(URLEncoder.encode(JSON.toJSONString(omsCartItems), "UTF-8"));
                 response.addCookie(cookie1);
             } else {
-                String cookieStr = URLDecoder.decode(cookie1.getValue(),"UTF-8");
+                String cookieStr = URLDecoder.decode(cookie1.getValue(), "UTF-8");
                 omsCartItems = JSON.parseArray(cookieStr, OmsCartItem.class);
                 for (OmsCartItem cartItem : omsCartItems) {
                     if (omsCartItem.getProductSkuId().equals(cartItem.getProductSkuId())) {
                         cartItem.setQuantity(cartItem.getQuantity() + omsCartItem.getQuantity());
-                        cartItem.setPrice(cartItem.getPrice().add(omsCartItem.getPrice()));
+                        cartItem.setPrice(cartItem.getPrice());
                         cartItem.setModifyDate(new Date());
                     } else {
                         omsCartItems.add(omsCartItem);
@@ -80,50 +83,78 @@ public class CartController {
                 }
                 cookie1.setMaxAge(60 * 60 * 24 * 3);
                 cookie1.setDomain("localhost");
-                cookie1.setValue(URLEncoder.encode(JSON.toJSONString(omsCartItems),"UTF-8"));
+                cookie1.setValue(URLEncoder.encode(JSON.toJSONString(omsCartItems), "UTF-8"));
                 cookie1.setPath("/");
                 response.addCookie(cookie1);
             }
-        }
-        else {
-           //说明用户已经登录
-            //需要先查看数据库是否有该商品和该用户的购物车的一个选项
-            OmsCartItem omsCartFromDb=cartItemService.getOmsCartItemByMemberIdAndSkuId(memberId,skuId);
-            if(omsCartFromDb==null){
-                //没有的话    做添加
+        } else {
+            OmsCartItem omsCartFromDb = cartItemService.getOmsCartItemByMemberIdAndSkuId(memberId, skuId);
+            if (omsCartFromDb == null) {
                 omsCartItem.setCreateDate(new Date());
                 omsCartItem.setMemberNickname("admin1");
                 omsCartItem.setMemberId(memberId);
                 omsCartItem.setProductId(skuInfo.getSpuId());
                 omsCartItems.add(omsCartItem);
                 cartItemService.saveCartItemToDB(omsCartItem);
-            }
-             else {
-                //有的话     做更新
-                omsCartFromDb.setPrice(omsCartFromDb.getPrice().add(omsCartItem.getPrice()));
-                omsCartFromDb.setQuantity(omsCartFromDb.getQuantity()+omsCartItem.getQuantity());
+            } else {
+                omsCartFromDb.setPrice(omsCartFromDb.getPrice());
+                omsCartFromDb.setQuantity(omsCartFromDb.getQuantity() + omsCartItem.getQuantity());
                 omsCartFromDb.setModifyDate(new Date());
                 omsCartFromDb.setProductId(skuInfo.getSpuId());
                 omsCartItems.add(omsCartFromDb);
                 cartItemService.updateCartItemBySkuId(omsCartFromDb);
             }
             Jedis jedis = redisUtil.getJedis();
-             String mapKey="user:"+memberId+":cart";
-            Map<String,String> map=new HashMap<>();
+            String mapKey = "user:" + memberId + ":cart";
+            Map<String, String> map = new HashMap<>();
             for (OmsCartItem cartItem : omsCartItems) {
                 String cartStr = JSON.toJSONString(cartItem);
-                map.put(cartItem.getProductSkuId(),cartStr);
+                map.put(cartItem.getProductSkuId(), cartStr);
             }
             try {
-                jedis.hmset(mapKey,map);
-            }catch (Exception e){
+                jedis.hmset(mapKey, map);
+            } catch (Exception e) {
                 e.printStackTrace();
                 jedis.close();
-            }finally {
+            } finally {
                 jedis.close();
             }
         }
         return "redirect:/success.html";
+    }
+
+    @RequestMapping("/cartList")
+    public String cartList(HttpServletResponse response, HttpServletRequest request, ModelMap modelMap) throws UnsupportedEncodingException {
+        List<OmsCartItem> omsCartItems = new ArrayList<>();
+        String memberId = "1";
+        Cookie cookie = new Cookie("cartListCookie", "");
+        if (StringUtils.isNotBlank(memberId)) {
+            omsCartItems = cartItemService.getCartItemByMemberId(memberId);
+            if (omsCartItems.size() > 0) {
+                for (OmsCartItem omsCartItem : omsCartItems) {
+                    omsCartItem.setTotalPrice(omsCartItem.getPrice().multiply(new BigDecimal(omsCartItem.getQuantity())).doubleValue());
+                    omsCartItems.add(omsCartItem);
+                }
+            }
+        } else {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null && cookies.length > 0) {
+                for (Cookie cookie1 : cookies) {
+                    if (cookie1.getName().equalsIgnoreCase(cookie.getName())) {
+                        cookie = cookie1;
+                    }
+                }
+            }
+            String cookieValue = cookie.getValue();
+            if (StringUtils.isNotBlank(cookieValue)) {
+                omsCartItems = JSON.parseArray(URLDecoder.decode(cookieValue, "UTF-8"), OmsCartItem.class);
+            } else {
+                cookie.setValue(URLEncoder.encode("", "UTF-8"));
+                response.addCookie(cookie);
+            }
+        }
+        modelMap.put("cartList", omsCartItems);
+        return "cartList.html";
     }
 
 
