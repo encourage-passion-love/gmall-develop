318b320004a23c428e5561dff504617d7927142c
JBF-XuFengpeng <951794968@qq.com>
2023-01-19 22:07:57 +0800

    通过自定义mvc模块进行加入拦截器mcv模块里面

---
+++ gmall-parent/gmall-cart-web/src/main/java/com/xfp/gmall/cart/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.xfp.gmall.cart.config;
+
+import com.xfp.gmall.cart.interceptors.HandlerInterceptorCart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private HandlerInterceptorCart handlerInterceptorCart;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(handlerInterceptorCart).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+}
