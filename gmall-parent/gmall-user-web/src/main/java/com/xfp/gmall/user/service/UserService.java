45d98667dc1b5c20155e11340ec658d53c57308a
JBF-XuFengpeng <951794968@qq.com>
2023-01-20 11:26:59 +0800

    用户登录的SQL编写

--- gmall-parent/gmall-user-web/src/main/java/com/xfp/gmall/user/service/UserService.java
+++ gmall-parent/gmall-user-web/src/main/java/com/xfp/gmall/user/service/UserService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface UserService {
     List<UmsMember> selectAllUser();
     List<UmsMemberReceiveAddress> selectAllUserReceiverAddress(long member_id);
+    UmsMember login(UmsMember umsMember);
 }
