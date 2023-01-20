5f4078a52bf64fdcf9603cf412ac131247e6e592
JBF-XuFengpeng <951794968@qq.com>
2023-01-20 11:28:20 +0800

    用户登录模块从redis里面或数据库里面获取数据，从而支持高并发

--- gmall-parent/gmall-user-service/src/main/java/com/xfp/gmall/user/service/UmsMemberServiceImpl.java
+++ gmall-parent/gmall-user-service/src/main/java/com/xfp/gmall/user/service/UmsMemberServiceImpl.java
@@ -1,11 +1,15 @@
 package com.xfp.gmall.user.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.xfp.gmall.user.bean.UmsMember;
 import com.xfp.gmall.user.bean.UmsMemberReceiveAddress;
 import com.xfp.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.xfp.gmall.user.mapper.UserMapper;
+import com.xfp.gmall.util.RedisUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 @Service
@@ -16,6 +20,8 @@ public class UmsMemberServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public List<UmsMember> selectAllUser() {
@@ -27,5 +33,37 @@ public class UmsMemberServiceImpl implements UserService {
     public List<UmsMemberReceiveAddress> selectAllUserReceiverAddress(long member_id) {
         List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectAllUserReceiverAddress(member_id);
         return umsMemberReceiveAddresses;
+    }
+
+    @Override
+    public UmsMember login(UmsMember umsMember) {
+        Jedis jedis=null;
+        try {
+            jedis= redisUtil.getJedis();
+            if(jedis!=null){
+                String userinfo = jedis.get("user:" + umsMember.getUsername() + ":info");
+                if(StringUtils.isNotBlank(userinfo)){
+                    UmsMember umsMember1 = JSON.parseObject(userinfo, UmsMember.class);
+                    return umsMember1;
+                }
+            }
+            UmsMember umsMemberFromDB=loginFromDB(umsMember);
+            if(umsMemberFromDB!=null){
+                //将数据放到redis里面去
+                jedis.setex("user:"+umsMember.getUsername()+":info",60*60*24,JSON.toJSONString(umsMemberFromDB));
+            }
+            return umsMemberFromDB;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            jedis.close();
+        }
+        return null;
+    }
+
+    private UmsMember loginFromDB(UmsMember umsMember) {
+        UmsMember selectByUsernameAndPassword = userMapper.selectByUsernameAndPassword(umsMember);
+        return selectByUsernameAndPassword;
     }
 }
