5771b2f5e50893a8cc703af1a1a698d631ec15cb
JBF-XuFengpeng <951794968@qq.com>
2023-01-20 11:26:00 +0800

    启动类扫描mapper接口

---
+++ gmall-parent/gmall-passport-web/src/main/java/com/xfp/gmall/passport/GmallPassportApp.java
@@ -0,0 +1,11 @@
+package com.xfp.gmall.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPassportApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPassportApp.class,args);
+    }
+}
