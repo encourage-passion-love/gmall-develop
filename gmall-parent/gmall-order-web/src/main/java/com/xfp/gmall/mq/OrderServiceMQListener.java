c66f600f8a5b3e83df817fc3227e807ef9544e92
JBF-XuFengpeng <951794968@qq.com>
2023-02-04 16:31:43 +0800

    监听队列支付成功信息进行更新订单状态信息

---
+++ gmall-parent/gmall-order-web/src/main/java/com/xfp/gmall/mq/OrderServiceMQListener.java
@@ -0,0 +1,24 @@
+package com.xfp.gmall.mq;
+
+import com.sun.org.apache.bcel.internal.generic.PUSH;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+
+@Component
+public class OrderServiceMQListener {
+
+    @JmsListener(destination = "PAYMENT_SUCCESS_QUEUE",containerFactory = "jmsQueueListener")
+    public void updateOrderProcessStatus(MapMessage mapMessage){
+        //使用JMS模块进行处理
+        try {
+            String out_trade_no = mapMessage.getString("out_trade_no");
+            //更新订单状态(根据订单交易码)
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
