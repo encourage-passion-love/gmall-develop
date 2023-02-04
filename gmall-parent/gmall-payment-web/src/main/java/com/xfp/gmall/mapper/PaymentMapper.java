74478b4aaa503a321d42166758eec95982de98bf
JBF-XuFengpeng <951794968@qq.com>
2023-02-04 16:03:42 +0800

    通用mapper的添加

---
+++ gmall-parent/gmall-payment-web/src/main/java/com/xfp/gmall/mapper/PaymentMapper.java
@@ -0,0 +1,9 @@
+package com.xfp.gmall.mapper;
+
+import com.xfp.gmall.entity.Payment;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.Map;
+
+public interface PaymentMapper extends Mapper<Payment> {
+}
