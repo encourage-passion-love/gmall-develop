a1c7bfbc502264fd9cf9df33089601c07a005d6c
JBF-XuFengpeng <951794968@qq.com>
2023-02-04 16:39:21 +0800

    SpuController内包路径修改

--- gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/SpuController.java
+++ gmall-parent/gmall-manager-web/src/main/java/com/xfp/gmall/manager/controller/SpuController.java
@@ -1,7 +1,7 @@
 package com.xfp.gmall.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.xfp.gmall.gmall.manager.bean.PmsProductInfo;
+import com.xfp.gmall.manager.bean.PmsProductInfo;
 import com.xfp.gmall.manager.service.SpuService;
 import com.xfp.gmall.manager.bean.PmsProductImage;
 import com.xfp.gmall.manager.bean.PmsProductSaleAttr;
