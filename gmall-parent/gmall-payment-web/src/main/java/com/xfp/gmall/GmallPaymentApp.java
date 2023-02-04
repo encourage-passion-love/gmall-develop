1e66a4b0dce1884cd352a29c75bc8c3423047167
JBF-XuFengpeng <951794968@qq.com>
2023-02-04 16:04:13 +0800

    扫描mapper接口文件

--- gmall-parent/gmall-payment-web/src/main/java/com/xfp/gmall/GmallPaymentApp.java
+++ gmall-parent/gmall-payment-web/src/main/java/com/xfp/gmall/GmallPaymentApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-//@MapperScan(basePackages = "com.xfp.gmall.mapper")
+@MapperScan(basePackages = "com.xfp.gmall.mapper")
 public class GmallPaymentApp {
     public static void main(String[] args) {
         SpringApplication.run(GmallPaymentApp.class,args);
