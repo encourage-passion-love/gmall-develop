e18fb170ffb59a41bf35382e94c7b450240680cd
JBF-XuFengpeng <951794968@qq.com>
2023-02-05 21:39:48 +0800

    使用thymeleaf返回页面

--- gmall-parent/gmall-shop-user/src/main/java/com/xfp/gmall/user/controller/UserController.java
+++ gmall-parent/gmall-shop-user/src/main/java/com/xfp/gmall/user/controller/UserController.java
@@ -5,13 +5,15 @@ import com.xfp.gmall.user.bean.UmsMember;
 import com.xfp.gmall.user.bean.UmsMemberReceiveAddress;
 import com.xfp.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("user")
 public class UserController {
 
@@ -19,12 +21,14 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("/all")
+    @ResponseBody
     public List<UmsMember> findAllUsers() {
         List<UmsMember> users = userService.selectAllUser();
         return users;
     }
 
     @RequestMapping("/getUserReceiveAddress/{member_id}")
+    @ResponseBody
     public List<UmsMemberReceiveAddress> findAllUsersReceiveAddress(@PathVariable("member_id") long member_id) {
         List<UmsMemberReceiveAddress> address = userService.
                 selectAllUserReceiverAddress(member_id);
